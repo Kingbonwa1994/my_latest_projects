380b56412009ffb7141778a56eafd00ef0b6e1ff
Kingbonwa1994 <bonwamdluli@gmail.com>
2024-08-20 19:06:09 +0200

    model optimized

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import  { Galaxy } from './App.tsx'
+
 import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Galaxy />
+    <App />
   </StrictMode>,
 )
